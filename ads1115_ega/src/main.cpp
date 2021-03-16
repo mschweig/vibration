83f5cced0e4caf63b79a849d61b27385d5818f2a
Schweiger Manuel <manuel.schweiger@a1.digital>
2021-03-16 21:22:20 +0100

    update

--- ads1115_ega/src/main.cpp
+++ ads1115_ega/src/main.cpp
@@ -6,7 +6,7 @@ Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 
 void setup(void)
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
  
   //Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
   
@@ -18,25 +18,25 @@ void setup(void)
   //                                                                -------  -------
   // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
   // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
-  ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
   // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
-  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-  
-  ads.begin();
-}
+  ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
 
+  ads.begin();
+  
+}
 void loop(void)
 {
   int16_t results;
   
   /* Be sure to update this value based on the IC and the gain settings! */
-  float multiplier = 0.1875F; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
+  float multiplier = 0.0078125F; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
 
   results = ads.readADC_Differential_0_1();  
     
-  //Serial.print("Differential: "); Serial.print(results); Serial.print("("); 
+  //Serial.print("Differential: "); Serial.print(results); Serial.print("("); @180 SPS
+  analogWrite(A0,100);
   Serial.println(results * multiplier);
 
-  delay(10);
 }
