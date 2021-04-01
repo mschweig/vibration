f0f245877e0f16e25c3e263575272341dc53f33b
Schweiger Manuel <manuel.schweiger@a1.digital>
2021-04-01 20:26:55 +0200

    added adxl343

--- ads1115_mega/src/main.cpp
+++ ads1115_mega/src/main.cpp
@@ -22,20 +22,32 @@ void setup(void)
   // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
-
   ads.begin();
-  
+
+  //start the vibration motor
+  analogWrite(A1,128);
+  delay(200);
 }
 void loop(void)
 {
   int16_t results;
-  
   /* Be sure to update this value based on the IC and the gain settings! */
   float multiplier = 0.0078125F; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
 
-  results = ads.readADC_Differential_0_1();  
-    
-  //Serial.print("Differential: "); Serial.print(results); Serial.print("("); @180 SPS
-  Serial.println(results * multiplier);
+  while(1){
 
+    if (millis() > 2000){
+      analogWrite(A1,0);
+      break;
+    }
+
+    results = ads.readADC_Differential_0_1();  
+
+    //Serial.print("Differential: "); Serial.print(results); Serial.print("(");
+
+    // ~ 450 Hz with Serial.println
+    Serial.println(results * multiplier);
+
+  }
+  
 }
